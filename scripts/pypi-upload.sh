0084d6a053e895953e59e28e719396e0e70d9e53
Patrick T. Komiske III <pkomiske@mit.edu>
2021-02-25 19:45:10 -0500

    fixing path error

--- scripts/pypi-upload.sh
+++ scripts/pypi-upload.sh
@@ -4,16 +4,16 @@
 if [ ! -z $PYPI_TEST ] || [ ! -z $PYPI ]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPITEST
   if [ $1 = "sdist" ]; then
-    twine upload -r testpypi --skip-existing ../dist/*.tar.gz
+    twine upload -r testpypi --skip-existing dist/*.tar.gz
   fi
-  twine upload -r testpypi --skip-existing ../wheelhouse/*.whl
+  twine upload -r testpypi --skip-existing wheelhouse/*.whl
 fi
 
 # upload to real pypi server
 if [ ! -z $PYPI ]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPI
   if [ $1 = "sdist" ]; then
-    twine upload --skip-existing ../dist/*.tar.gz
+    twine upload --skip-existing dist/*.tar.gz
   fi
-  twine upload --skip-existing ../wheelhouse/*.whl
+  twine upload --skip-existing wheelhouse/*.whl
 fi
