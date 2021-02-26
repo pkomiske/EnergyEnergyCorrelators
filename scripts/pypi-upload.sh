039a054262bdab9bae75df35af1845bc5aae9f23
Patrick T. Komiske III <pkomiske@mit.edu>
2021-02-25 22:53:04 -0500

    fixed twine path for mac

--- scripts/pypi-upload.sh
+++ scripts/pypi-upload.sh
@@ -4,16 +4,16 @@
 if [ ! -z $PYPI_TEST ] || [ ! -z $PYPI ]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPITEST
   if [ "$1" = "sdist" ]; then
-    twine upload -r testpypi --skip-existing dist/*.tar.gz
+    python3 -m twine upload -r testpypi --skip-existing dist/*.tar.gz
   fi
-  twine upload -r testpypi --skip-existing wheelhouse/*.whl
+  python3 -m twine upload -r testpypi --skip-existing wheelhouse/*.whl
 fi
 
 # upload to real pypi server
 if [ ! -z $PYPI ]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPI
   if [ "$1" = "sdist" ]; then
-    twine upload --skip-existing dist/*.tar.gz
+    python3 -m twine upload --skip-existing dist/*.tar.gz
   fi
-  twine upload --skip-existing wheelhouse/*.whl
+  python3 -m twine upload --skip-existing wheelhouse/*.whl
 fi
